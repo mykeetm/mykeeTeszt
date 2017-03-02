0e9ef78ce7814ff03e1edf65112f6b1ac6390fb9
mykee <bazso.miklos@gmail.com>
2017-03-02 17:19:27 +0100

    My first test commit

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<?
-include "include/config.php";
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mykee
+ * Date: 2017. 03. 02.
+ * Time: 17:18
+ */
 
+include "include/config.php";
 
 echo "hello world";
 
